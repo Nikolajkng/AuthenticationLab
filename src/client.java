3e404926fa70075fa5253bd9ecbf786008d8986a
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-10-30 13:44:30 +0100

    structure

--- src/client.java
+++ src/client.java
@@ -1,3 +1,4 @@
+package src;
 public class client {
 public static void main(String[] args) {
     
---
+++ src/sessionManager.java
@@ -0,0 +1,7 @@
+package src;
+public class sessionManager {
+    public static void main(String[] args) {
+    
+    }
+    
+}
